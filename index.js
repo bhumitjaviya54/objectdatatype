a4cd52b9599c79744e39e431c145a5d0a666659e
bhumitjaviya54 <93394436+bhumitjaviya54@users.noreply.github.com>
2023-12-29 10:47:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Object Data Type
+
+// In JS, an object is a collection of properties.
+
+// where each property is define as a key-value pair.
+
+let person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+console.log(person);
+console.log(typeof person);
